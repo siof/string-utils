c70d2e1d367f93beaea9cf108ccfcb3a2edd701d
siof <siof@siof.pl>
2014-06-14 23:06:21 +0200

    init

---
+++ trim.hpp
@@ -0,0 +1,22 @@
+#ifndef SIOF_STD_STRING_TRIM
+#define SIOF_STD_STRING_TRIM
+
+#include <string>
+
+namespace siof
+{
+    template<class T>
+    static inline std::basic_string<T>& trim(const std::basic_string<T> & str)
+    {
+        size_t first = str.find_first_not_of(' ');
+        size_t last = str.find_last_not_of(' ');
+        return str.substr(first, last - first + 1);
+    }
+
+    template<class T>
+    static inline void trim(std::basic_string<T> & str)
+    {
+        str = trim(const_cast<const std::basic_string<T>& >(str));
+    }
+}
+#endif // SIOF_STD_STRING_TRIM
