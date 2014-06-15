ac90e83c36b62696b45683dd90dee3d48637a778
siof <siof@siof.pl>
2014-06-15 15:44:25 +0200

    Licence

--- split.hpp
+++ split.hpp
@@ -1,3 +1,14 @@
+/*
+*      "THE BEER-WARE LICENSE" (Revision 42):
+*      Siof <siof@siof.pl> wrote this file. As long as you retain this notice you
+*      can do whatever you want with this stuff. If we meet some day, and you think
+*      this stuff is worth it, you can buy me a beer in return.
+*
+*      File:       split.hpp
+*      Author:     siof@siof.pl
+*      Date:       15/06/2014
+*/
+
 #ifndef SIOF_STRING_UTILS_SPLIT
 #define SIOF_STRING_UTILS_SPLIT
 
